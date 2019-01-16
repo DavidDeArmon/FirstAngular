3efa59a58ca4da719be2cdd39d223d872b0e86b1
David DeArmon <david.dearmon@gmail.com>
2019-01-15 17:43:52 -0700

    finished routing and details page

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,21 +9,14 @@ import { HeroService } from'../hero.service'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
   constructor(private heroService:HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  hero: Hero = {
-    id:1,
-    name: 'Windstorm'
-  }
-  heroes: Hero[];
-  selectedHero:Hero;
-  onSelect(hero:Hero):void{
-    this.selectedHero = hero;
-  }
   getHeroes(): void{
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
